43d10a65348a106d8603b998d4e3ca592b17245b
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-10-21 19:13:37 +0100

    add missing props

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -33,6 +33,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
                 `,
                 className
             )}
+            {...props}
         >
             {children}
         </button>
