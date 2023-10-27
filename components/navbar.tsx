ccb46933f7a5ee3eebebd8e0fd70e31bceec3af3
Elisabete Costa <97245586+elisabetecosta@users.noreply.github.com>
2023-10-27 18:31:35 +0100

    Update store name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = async () => {
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex items-center h-16">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                        <p className="font-bold text-xl">STORE</p>
+                        <p className="font-bold text-xl">KOREAN STORE</p>
                     </Link>
                     <MainNav data={categories} />
                     <NavbarActions />
